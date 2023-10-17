ebece4413de038614a9bc83574356c9397e9f883
Thom204 <tmonnier@unal.edu.co>
2023-10-17 11:36:11 -0500

    corregir toString

--- futbolista.py
+++ futbolista.py
@@ -31,4 +31,4 @@ class Futbolista(Persona, Deportista):
         self.__piernaHabil = piernaHabil
 
     def __str__(self):
-        return f"Mi nombre es {self.getNombre()}, soy profesional en el deporte {self.getDeporte()}. Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
+        return f"Mi nombre es {self.getNombre()} soy profesional en el deporte {self.getDeporte()} Tengo {self.getEdad()} años de edad y llevo {self.getAñosPracticando()} años en el deporte"
